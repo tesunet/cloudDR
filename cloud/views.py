1c210688c95cf8a2a0fd57457e6bbf152e0daed9
miaokela <2972799448@qq.com>
2018-05-29 17:18:40 +0800

    test

--- cloud/views.py
+++ cloud/views.py
@@ -30,7 +30,7 @@ from cloud.vmApi import *
 
 info = {"webaddr": "cv-server", "port": "81", "username": "admin", "passwd": "Admin@2017", "token": "",
         "lastlogin": 0}
-#2
+
 def index(request):
     if request.user.is_authenticated():
         cvvendor = Vendor.objects.filter(name='CommVault')
