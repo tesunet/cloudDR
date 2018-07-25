ae8bdd28eb5563d101b86b01c4af00f04cbae46d
缪克拉 <2972799448@qq.com>
2018-07-25 22:34:27 +0800

    完善remote.py中处理脚本错误的措施； 1.linux下脚本语法错误,或者命令不存在等没有通过stderr变量接收到，只能添加判断条件； 2.windows下可以接收到错误信息并作出判断；

--- cloud/remote.py
+++ cloud/remote.py
@@ -1,5 +1,7 @@
 """
 paramiko, pywinrm实现windows/linux脚本调用
+linux下脚本语法错误,或者命令不存在等没有通过stderr变量接收到，只能添加判断条件；
+windows下可以接收到错误信息并作出判断；
 """
 import paramiko
 import winrm
@@ -18,16 +20,34 @@ class ServerByPara(object):
     def exec_linux_cmd(self):
         data_init = ''
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect(hostname=self.host, username=self.user, password=self.pwd)
-        stdin, stdout, stderr = self.client.exec_command(self.cmd, get_pty=True)
+        try:
+            self.client.connect(hostname=self.host, username=self.user, password=self.pwd)
+        except:
+            print("连接服务器失败")
+            return {
+            "exec_tag": 1,
+            "data": "连接服务器失败",
+        }
+        try:
+            stdin, stdout, stderr = self.client.exec_command(self.cmd, get_pty=True)
+        except:
+            print("脚本执行超时")
+            return {
+            "exec_tag": 1,
+            "data": "脚本执行超时",
+        }
         if stderr.readlines():
             exec_tag = 1
-            for data in stdout.readlines():
+            for data in stderr.readlines():
                 data_init += data
         else:
             exec_tag = 0
             for data in stdout.readlines():
                 data_init += data
+            if "command not found" in data_init:  # 命令不存在
+                exec_tag = 1
+            elif "syntax error" in data_init:  # 语法错误
+                exec_tag = 1
         return {
             "exec_tag": exec_tag,
             "data": data_init,
@@ -35,8 +55,15 @@ class ServerByPara(object):
 
     def exec_win_cmd(self):
         data_init = ""
-        s = winrm.Session(self.host, auth=(self.user, self.pwd))
-        ret = s.run_cmd(self.cmd)
+        try:
+            s = winrm.Session(self.host, auth=(self.user, self.pwd))
+            ret = s.run_cmd(self.cmd)
+        except:
+            print("连接服务器失败")
+            return {
+            "exec_tag": 1,
+            "data": "连接服务器失败",
+        }
         if ret.std_err.decode():
             exec_tag = 1
             for data in ret.std_err.decode().split("\r\n"):
@@ -56,13 +83,10 @@ class ServerByPara(object):
         else:
             result = self.exec_win_cmd()
         print(result)
-        print(result["data"])
         return result
 
-# if __name__ == '__main__':
-# server_obj = ServerByPara(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-# server_obj = ServerByPara(r"C:\Users\a\Desktop\test.bat", "192.168.12.149", "a",
-#                           "zxcvbn123", "Windows")
-# server_obj = ServerByPara(r"/root/Desktop/test.sh >> log.txt", "192.168.109.132", "root",
-#                           "123456", "Linux")
-# server_obj.run()
+#if __name__ == '__main__':
+	#server_obj = ServerByPara(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+	#server_obj = ServerByPara(r"C:\Users\Administrator\Desktop\test0.bat", "192.168.100.153", "administrator","tesunet@2017", "Windows")
+	#server_obj = ServerByPara(r"/root/Desktop/test06.sh hello", "47.95.195.90", "root","!zxcvbn123", "Linux")
+	#server_obj.run()
